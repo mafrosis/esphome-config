7103b1c6846c93d2ec0fbc86251af28c42f7e3bf
Matt Black <github@mafro.net>
2022-02-12 15:10:13 +1100

    Package render script and add click

--- esphome_render/render.py
+++ esphome_render/render.py
@@ -1,23 +1,24 @@
-#! /usr/bin/env python3
-
 import os
 
+import click
 from jinja2 import Environment, FileSystemLoader
 
 
-def main():
-    device_id = os.environ['ID']
-    device_name = os.environ['NAME']
-    static_ip = os.environ['IP']
-    template = os.environ['TMPL']
+@click.command()
+@click.argument('device_id')
+@click.argument('device_ip')
+@click.argument('device_type')
+@click.argument('name')
+@click.argument('templates', nargs=-1)
+@click.option('--room', default='nil', help='Name of the room for this sensor')
+def main(device_id: str, device_ip: str, device_type: str, name: str, templates, room: str=None):
     wifi_password = os.environ['WIFI_PASSWORD'].strip()
     fallback_password = os.environ['WIFI_FALLBACK'].strip()
-    room = os.environ.get('ROOM', '').strip()
 
-    if 'esp32' in template:
+    if device_type == 'esp32':
         platform = 'ESP32'
         board = 'nodemcu-32s'
-    elif 'esp8266' in template:
+    elif device_type == 'esp8266':
         platform = 'ESP8266'
         board = 'nodemcuv2'
     else:
@@ -26,6 +27,8 @@ def main():
 
     if not wifi_password:
         raise Exception('WIFI_PASSWORD env not set')
+    if not fallback_password:
+        raise Exception('FALLBACK_PASSWORD env not set')
 
     env = Environment(loader=FileSystemLoader('templates'))
 
@@ -34,8 +37,8 @@ def main():
         t = env.from_string(f.read())
         esphome = t.render(
             device_id=device_id,
-            device_name=device_name,
-            static_ip=static_ip,
+            device_name=name,
+            static_ip=device_ip,
             wifi_password=wifi_password,
             fallback_password=fallback_password,
             platform=platform,
@@ -43,20 +46,14 @@ def main():
             room=room,
         )
 
-    # Read the device specific configuration
-    with open(f'templates/{template}.tmpl', encoding='utf-8') as f:
-        device = f.read()
-
-    # Combine the header and device specific config
-    output = f'{esphome}\n\n# ----------------------------------------\n\n{device}'
+    # Combine the esphome header with each template
+    for template in templates:
+        with open(f'templates/{template}.tmpl', encoding='utf-8') as f:
+            esphome = f'{esphome}\n\n# ----------------------------------------\n\n{f.read()}'
 
     if not os.path.exists('build'):
         os.makedirs('build')
 
     # Write out the build config
-    with open(f'build/device_{device_id}.yaml', 'w') as f:
-        f.write(output)
-
-
-if __name__ == '__main__':
-    main()
+    with open(f'build/device_{device_id}.yaml', 'w', encoding='utf-8') as f:
+        f.write(esphome)
